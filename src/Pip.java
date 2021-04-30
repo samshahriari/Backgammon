7940557291f498b4e897dd6ac21f47626aaaf794
Jordan <jordanjo@kth.se>
2021-04-30 13:13:49 +0200

    Add Pip class and basic methods

--- src/Pip.java
+++ src/Pip.java
@@ -1,2 +1,26 @@
+import java.util.Stack;
+
 public class Pip {
+
+    private Stack<Checker> checkerStack;
+
+    public Pip() {
+        checkerStack = new Stack<Checker>();
+    }
+
+    public void addChecker(Checker checker) {
+        checkerStack.push(checker);
+    }
+
+    public Checker removeChecker() {
+        return checkerStack.pop();
+    }
+
+    public boolean isOccupied() {
+        return !checkerStack.isEmpty();
+    }
+
+    public boolean isStacked() {
+        return checkerStack.size() > 1;
+    }
 }
