39aff7dd9e3c07b2531ff6c67127753108f2c6c6
Jordan <jordanjo@kth.se>
2021-04-30 16:06:07 +0200

    Add javadoc for Checker and make COLOR final

--- src/Checker.java
+++ src/Checker.java
@@ -1,30 +1,60 @@
+/**
+ * Class for checkers used on a backgammon board.
+ * Contains information about checker color, position, and direction.
+ *
+ * @author  Jordan & Sam
+ * @version 2021-04-30
+ */
 public class Checker {
 
-    private Color color;
+    private final Color COLOR;  // color of checker
 
-    private Pip position;
+    private Pip position;  // position of checker
 
+    /**
+     * Create new checker.
+     *
+     * @param c The color of the new checker
+     * @param pos The position of the new checker
+     */
     public Checker(Color c, Pip pos) {
-        color = c;
+        COLOR = c;
         position = pos;
     }
 
-    // Prints the color of the checker instead of memory address
+    /**
+     * @return String representation of the checker (returns the color of the checker)
+     */
     @Override
     public String toString() {
-        return color.toString();
+        return COLOR.toString();
     }
 
+    /**
+     * Get the color of a checker.
+     *
+     * @return Color of checker: WHITE/RED
+     */
     public Color getColor() {
-        return color;
+        return COLOR;
     }
 
+    /**
+     * Get position of a checker.
+     *
+     * @return Pip position of checker
+     */
     public Pip getPosition() {
         return position;
     }
 
+    /**
+     * Get the movement direction associated with a checker color.
+     *
+     * @return int 1 for WHITE direction and -1 for RED direction
+     */
     public int getDirection() {
-        if (color == Color.WHITE) {
+        if (COLOR == Color.WHITE) {
             return 1;
         }
         return -1; // Direction for red
