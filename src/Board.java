2163547bc33ae1154ac7b70b5e35fa40449fdaf5
Jordan <jordanjo@kth.se>
2021-04-30 11:25:29 +0200

    Add Board constructor

--- src/Board.java
+++ src/Board.java
@@ -1,2 +1,21 @@
 public class Board {
+
+    public enum Player {
+        WHITE, RED
+    }
+
+    private Pip[] pips = new Pip[24]; // 0-23
+    private int redBar;
+    private int whiteBar;
+
+    public Board() {
+        for (int i = 0; i < 24; i++) {
+            pips[i] = new Pip();
+        }
+    }
 }
+
+    // pips
+    // quadrants
+    // checkers
+    // players
