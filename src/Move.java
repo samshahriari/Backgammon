f3603bc34ddb41021a45869176320be25e0e262c
Sam Shahriari <samsha@kth.se>
2021-04-30 14:46:08 +0200

    Add helper methods for Move

--- src/Move.java
+++ src/Move.java
@@ -10,4 +10,22 @@ public class Move {
         newPip.addChecker(currentPip.removeChecker());
     }
 
+    private boolean outOfBounds(int newPipIndex) {
+        return newPipIndex < 0 || newPipIndex > 23;
+    }
+
+    /**
+     * @param pip
+     * @param color
+     * @return -1 if pip is empty or ally checker, 0 if lone enemy checker, 1 if blocked by enemy stack
+     */
+    private int checkPip(Pip pip, Color color) {
+        if (!pip.isOccupied() || pip.getColor() == color) {
+            return -1;
+        } else if (!pip.isStacked()) {
+            return 0; // is naked and afraid
+        } else {
+            return 1; // is stacked
+        }
+    }
 }
