51ffeed297502d271b8291fb36baad958c8ddf8b
Sam Shahriari <samsha@kth.se>
2021-05-06 14:23:40 +0200

    Add javadoc for Board class

--- src/Board.java
+++ src/Board.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
  * Contains information about pips, bars, [...]
  *
  * @author  Jordan & Sam
- * @version 2021-04-30
+ * @version 2021-05-06
  */
 public class Board {
 
@@ -29,22 +29,23 @@ public class Board {
      *
      * Initial checker set up
      *
-     * --  --  --  --  --  --           --  --  --  --  --  --
-     * 12  11  10  09  08  07   B A R   06  05  04  03  02  01
-     * W               R         < >    R                   W
-     * W               R                R                   W
-     * W               R                R
-     * W                                R
-     * W                                R
+     *      --  --  --  --  --  --           --  --  --  --  --  --
+     *      12  11  10  09  08  07   B A R   06  05  04  03  02  01
+     *      W               R         < >    R                   W
+     *      W               R                R                   W
+     *      W               R                R
+     *      W                                R
+     *      W                                R
      *
      *
-     * R                               W
-     * R                               W
-     * R               W               W
-     * R               W               W                   R
-     * R               W        < >    W                   R
-     * 13  14  15  16  17  18  B A R   19  20  21  22  23  24
-     * --  --  --  --  --  --          --  --  --  --  --  --
+     *      R                               W
+     *      R                               W
+     *      R               W               W
+     *      R               W               W                   R
+     *      R               W        < >    W                   R
+     *      13  14  15  16  17  18  B A R   19  20  21  22  23  24
+     *      --  --  --  --  --  --          --  --  --  --  --  --
+     *
      */
     public void setUpCheckers() {
 
@@ -61,6 +62,11 @@ public class Board {
         pips.get(24).setCheckers(2, Color.RED);
     }
 
+    /**
+     * Sums and returns the count of white checkers at the board (excluding the bar).
+     *
+     * @return The total number of white checkers at the board
+     */
     public int getWhiteCheckerCount() {
         int checkerCount = 0;
         for (int i = 1; i < 25; i++) {
@@ -71,6 +77,11 @@ public class Board {
         return checkerCount;
     }
 
+    /**
+     * Sums and returns the count of red checkers at the board (excluding the bar).
+     *
+     * @return The total number of red checkers at the board
+     */
     public int getRedCheckerCount() {
         int checkerCount = 0;
         for (int i = 1; i < 25; i++) {
@@ -81,6 +92,12 @@ public class Board {
         return checkerCount;
     }
 
+    /**
+     * Get the pip that symbolizes the bar for the given color.
+     *
+     * @param color The color of the bar
+     * @return Bar Pip corresponding to the color.
+     */
     public Pip getBar(Color color) {
         int barIndex = 0;  // zero if WHITE
         if (color == Color.RED) {
@@ -92,7 +109,7 @@ public class Board {
     /**
      * Get the index of a pip.
      *
-     * @param pip the pip whose index is sought
+     * @param pip The pip whose index is sought
      * @return int The index of a pip
      */
     public int getPipIndex(Pip pip) {
@@ -109,27 +126,36 @@ public class Board {
         return pips.get(index);
     }
 
+    /**
+     * Print the board, including the checkers, pips, bars and indices, of the given game to the terminal.
+     *
+     * @param game The game that should be printed
+     */
     public void displayBoard(Game game) {
 
         System.out.println("\nWBO: " + game.getBearingOffStatus(Color.WHITE));
         System.out.println("RBO: " + game.getBearingOffStatus(Color.RED));
 
+
+        // Upper half of the board
         System.out.println("\n12 11 10 09 08 07         06 05 04 03 02 01");
         System.out.println("-- -- -- -- -- --  B A R  -- -- -- -- -- --");
         for (int i = 12; i >= 1; i--) {
             System.out.print(pips.get(i) + " ");
-            if (i == 7) {
-                System.out.print("  " + Pip.ANSI_WHITE_BACKGROUND + Pip.ANSI_BLACK + "<");
-                System.out.print(pips.get(0).getCheckerCount());
+            if (i == 7) { // between the red outer and home quadrant
+                System.out.print("  " + Pip.ANSI_WHITE_BACKGROUND + Pip.ANSI_BLACK + "<"); // colors the bar white
+                System.out.print(pips.get(0).getCheckerCount()); // the white bar
                 System.out.print(">"+ Pip.ANSI_RESET + "   ");
             }
         }
         System.out.println("\n\n\n");
+
+        // Lower half of the board
         for (int i = 13; i <= 24; i++) {
             System.out.print(pips.get(i) + " ");
-            if (i == 18) {
-                System.out.print("  " + Pip.ANSI_RED_BACKGROUND + Pip.ANSI_BLACK + "<");
-                System.out.print(pips.get(25).getCheckerCount());
+            if (i == 18) { // between the white outer and home quadrant
+                System.out.print("  " + Pip.ANSI_RED_BACKGROUND + Pip.ANSI_BLACK + "<"); // colors the bar red
+                System.out.print(pips.get(25).getCheckerCount()); // the red bar
                 System.out.print(">"+ Pip.ANSI_RESET + "   ");
             }
         }
