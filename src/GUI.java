ebbb31e21d9f5ab0bdb52ceeaf8d65512473074e
Sam Shahriari <samsha@kth.se>
2021-05-20 10:46:56 +0200

    Add javadoc

--- src/GUI.java
+++ src/GUI.java
@@ -12,6 +12,8 @@ import javax.swing.*;
  */
 public class GUI {
 
+
+    // Coordinates and sizes constants for components on the board.
     public static final int BOARD_HEIGHT = 580;
     public static final int BOARD_WIDTH = 664;
     public static final int BEZEL = 10;
@@ -32,12 +34,12 @@ public class GUI {
     public static final int DICE_TRAY_LEFT = OUTER_LEFT_EDGE - BOARD_DICE_DISTANCE - DICE_TRAY_WIDTH;
     public static final int DICE_TRAY_RIGHT = DICE_TRAY_LEFT + DICE_TRAY_WIDTH;
     public static final int DICE_TRAY_TOP = BOARD_Y_AXIS - DICE_TRAY_HEIGHT/2;
-    public static final int TITLE_BAR_HEIGHT = 30;
     public static final int OUTER_RIGHT_EDGE = OUTER_LEFT_EDGE + BOARD_WIDTH;
     public static final int OUTER_BOTTOM_EDGE = OUTER_TOP_EDGE + BOARD_HEIGHT;
     public static final int INNER_RIGHT_EDGE = OUTER_RIGHT_EDGE - BEZEL;
     public static final int INNER_BOTTOM_EDGE = OUTER_BOTTOM_EDGE - BEZEL;
 
+    // Colors used in the game
     private static final java.awt.Color RED_PIP = new java.awt.Color(255, 44, 19);
     private static final java.awt.Color WHITE_PIP = new java.awt.Color(255, 221, 215);
     private static final java.awt.Color BOARD_COLOR = new java.awt.Color(213, 123, 77);
@@ -49,6 +51,7 @@ public class GUI {
     private static final java.awt.Color WHITE_CHECKER = new java.awt.Color(238, 225, 203);
     private static final java.awt.Color WHITE_CHECKER_BORDER = new java.awt.Color(196, 176, 159);
 
+    // Panels on the GUI
     JFrame window;
     DiceTrayP diceTrayPanel;
     DiceFacesP diceFacesPanel;
@@ -65,7 +68,7 @@ public class GUI {
         window = new JFrame();
         window.setSize(1280, 720);
         window.setTitle("Backgammon");
-        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // the game closes when you press X
         window.setLocationRelativeTo(null);
         window.setResizable(false);
 
@@ -88,6 +91,8 @@ public class GUI {
         chkInGoalPanel = new CheckersInGoalP();
         activePlayerPanel = new ActivePlayerP();
 
+        // Add the panels to the frame
+        // The panel added first will be on top
         base.add(activePlayerPanel, 0, -1);
         base.add(chkInGoalPanel, 0, -1);
         base.add(checkersPanel, 0, -1);
@@ -103,6 +108,9 @@ public class GUI {
         window.setVisible(true);
     }
 
+    /**
+     * Paints the outer board.
+     */
     private class BoardP extends JPanel {
         public BoardP() {
             setBounds( OUTER_LEFT_EDGE,  OUTER_TOP_EDGE,  BOARD_WIDTH,  BOARD_HEIGHT );
@@ -111,6 +119,9 @@ public class GUI {
         }
     }
 
+    /**
+     * Paints the bar
+     */
     private class BarP extends JPanel {
         public BarP() {
             setBounds( BOARD_X_AXIS-(BAR_WIDTH/2),  OUTER_TOP_EDGE,  BAR_WIDTH,BOARD_HEIGHT );
@@ -119,6 +130,9 @@ public class GUI {
         }
     }
 
+    /**
+     * Paints the dice tray
+     */
     private class DiceTrayP extends JPanel {
         public DiceTrayP() {
             setBounds( DICE_TRAY_LEFT,  DICE_TRAY_TOP,  DICE_TRAY_WIDTH,  DICE_TRAY_HEIGHT );
@@ -128,6 +142,9 @@ public class GUI {
         }
     }
 
+    /**
+     * Paints  the checkers in the goal
+     */
     public class CheckersInGoalP extends JPanel {
 
         int whiteCheckersInGoal;
@@ -138,6 +155,12 @@ public class GUI {
             setOpaque(false);
         }
 
+        /**
+         * Update the fields that keeps track of the checkers in white's and red's goal and paint the new checker.
+         *
+         * @param whiteCheckers The number of white checkers in goal
+         * @param redCheckers The number of red checkers in goal
+         */
         public void updateCheckers(int whiteCheckers, int redCheckers) {
             whiteCheckersInGoal = whiteCheckers;
             redCheckersInGoal = redCheckers;
@@ -159,19 +182,22 @@ public class GUI {
             g2.setColor(RED_CHECKER);
             int yp = BEZEL+initialPadding;
             for (int i = 0; i < redCheckersInGoal; i++) {
-                g2.fillRect(xp,yp, CHECKER_DIAMETER, checkerThickness);
+                g2.fillRect(xp,yp, CHECKER_DIAMETER, checkerThickness); // paint the red checkers
                 yp += checkerThickness + checkerSpacing;
             }
             // WHITE settings
             g2.setColor(WHITE_CHECKER);
-            yp = BOARD_HEIGHT - BEZEL - checkerThickness - initialPadding;
+            yp = BOARD_HEIGHT - BEZEL - checkerThickness - initialPadding; // bottom of the red goal
             for (int i = 0; i < whiteCheckersInGoal; i++) {
-                g2.fillRect(xp,yp, CHECKER_DIAMETER, checkerThickness);
+                g2.fillRect(xp,yp, CHECKER_DIAMETER, checkerThickness); // paint the white checkers
                 yp -= checkerThickness + checkerSpacing;
             }
         }
     }
 
+    /**
+     * Paints and updates the checkers on the pips and bar.
+     */
     public class CheckersP extends JPanel {
 
         ArrayList<Pip> pips;
@@ -182,16 +208,29 @@ public class GUI {
             setOpaque(false);
         }
 
+        /**
+         * Updates the internal list of where the checkers are placed.
+         *
+         * @param newPips A updated list of the checkers placement
+         */
         public void updatePips(ArrayList<Pip> newPips) {
             pips = newPips;
             repaint();
         }
 
+        /**
+         * Highlights the top checker of a pip.
+         *
+         * @param pipIndex The index of the pip whose last checker should be highlighted
+         */
         public void highlightChecker(int pipIndex) {
             highlightPipIndex = pipIndex;
             repaint();
         }
 
+        /**
+         * Remove the highlight for the highlighted checker.
+         */
         public void resetHighlight() {
             highlightPipIndex = -1;
         }
@@ -202,10 +241,10 @@ public class GUI {
             // Paint the checkers in upper quadrants
             int index = 12;
             for (int x = 0; x < INNER_RIGHT_EDGE-INNER_LEFT_EDGE; x += PIP_WIDTH) {
-                if (index == 6) { // paint the bar
+                if (index == 6) { // set up for painting the bar
                     index = 0;
                     x += BEZEL;
-                } else if (index == -1) { // go back to pip
+                } else if (index == -1) { // go back to paint the pips
                     index = 6;
                     x += BEZEL;
                 }
@@ -213,8 +252,8 @@ public class GUI {
                 int checkersPlaced = 0;
                 Color checkerColor = pips.get(index).getColor() == Player.WHITE ? WHITE_CHECKER : RED_CHECKER;
                 Color checkerBorderColor = pips.get(index).getColor() == Player.WHITE ? WHITE_CHECKER_BORDER : RED_CHECKER_BORDER;
-                for (int y = 0; checkersPlaced < checkersOnPip; y += PIP_WIDTH - 1) {
-                    switch (checkersPlaced) {
+                for (int y = 0; checkersPlaced < checkersOnPip; y += PIP_WIDTH - 1) { // paints checkers until all checkers on pip has been painted
+                    switch (checkersPlaced) { // adjust y-axis when pip becomes full
                         case 5:
                             y = PIP_WIDTH/2;
                             break;
@@ -248,10 +287,10 @@ public class GUI {
             // Paint the checkers in lower quadrants
             index = 13;
             for (int x = 0; x < INNER_RIGHT_EDGE-INNER_LEFT_EDGE; x += PIP_WIDTH) {
-                if (index == 19) {
+                if (index == 19) { // set up for painting the bar
                     index = 25;
                     x += BEZEL;
-                } else if (index == 26) {
+                } else if (index == 26) { // go back to paint the pips
                     index = 19;
                     x += BEZEL;
                 }
@@ -260,8 +299,8 @@ public class GUI {
                 Color checkerColor = pips.get(index).getColor() == Player.WHITE ? WHITE_CHECKER : RED_CHECKER;
                 Color checkerBorderColor = pips.get(index).getColor() == Player.WHITE ? WHITE_CHECKER_BORDER : RED_CHECKER_BORDER;
                 int startPos = BOARD_HEIGHT- 2*BEZEL-PIP_WIDTH;
-                for (int y = startPos; checkersPlaced < checkersOnPip; y -= PIP_WIDTH - 1) {
-                    switch (checkersPlaced) {
+                for (int y = startPos; checkersPlaced < checkersOnPip; y -= PIP_WIDTH - 1) { // paints checkers until all checkers on pip has been painted
+                    switch (checkersPlaced) { // adjust y-axis when pip becomes full
                         case 5:
                             y = startPos - PIP_WIDTH/2;
                             break;
@@ -294,6 +333,9 @@ public class GUI {
         }
     }
 
+    /**
+     * Paints the board.
+     */
     public class GoalP extends JPanel {
         public GoalP() {
             setBounds(OUTER_RIGHT_EDGE + BOARD_DICE_DISTANCE / 2, OUTER_TOP_EDGE, CHECKER_DIAMETER + 30, BOARD_HEIGHT);
@@ -301,21 +343,31 @@ public class GUI {
             setBorder(BorderFactory.createLineBorder(BORDER_COLOR, BEZEL));
         }
 
+        /**
+         * Calculates where the mouse has clicked.
+         *
+         * @param x The x coordinate
+         * @param y The y coordinate
+         * @return -1 if clicked outside of goal, 0 if clicked on red's goal and 25 if clicked on white's goal
+         */
         public int mouseOnGoalIndex(int x, int y) {
             int pipIndex = -1;
             x -= OUTER_RIGHT_EDGE + BOARD_DICE_DISTANCE / 2;
             y -= OUTER_TOP_EDGE;
             if (x > BEZEL && x < CHECKER_DIAMETER + 30 - BEZEL) {
                 if (y > BEZEL && y < BOARD_HEIGHT / 2 - 5) {
-                    pipIndex = 0;
+                    pipIndex = 0; // red's goal
                 } else if (y > INNER_BOTTOM_EDGE - BOARD_HEIGHT / 2 + 5 && y < INNER_BOTTOM_EDGE) {
-                    pipIndex = 25;
+                    pipIndex = 25; // white's goal
                 }
             }
             return pipIndex;
         }
     }
 
+    /**
+     * Paints the pips.
+     */
     public class PipsP extends JPanel {
 
         private int[] xp;
@@ -325,31 +377,38 @@ public class GUI {
             setBounds( INNER_LEFT_EDGE,  INNER_TOP_EDGE,  BOARD_WIDTH,  BOARD_HEIGHT );
         }
 
+        /**
+         * Calculates where the mouse has clicked.
+         *
+         * @param x The x coordinate
+         * @param y The y coordinate
+         * @return -1 if clicked outside of goal, otherwise the index of the pip/bar (0--25)
+         */
         public int mouseOnPipIndex(int x, int y) {
             int pipIndex = -1;
             x -= INNER_LEFT_EDGE;
             if (y > INNER_TOP_EDGE && y < INNER_TOP_EDGE + 5*(CHECKER_DIAMETER+3)) {
                 if (x > 0 && x < 6*PIP_WIDTH) {
-                    pipIndex = 12-x/PIP_WIDTH;
+                    pipIndex = 12-x/PIP_WIDTH; // red outer quadrant
                 }
                 else if (x > 6*PIP_WIDTH && x < 6*PIP_WIDTH + BAR_WIDTH) {
-                    pipIndex = 0;
+                    pipIndex = 0; // white's bar
                 }
                 else if (x > 6*PIP_WIDTH + BAR_WIDTH && x < 12*PIP_WIDTH + BAR_WIDTH) {
                     x -= 6*PIP_WIDTH + BAR_WIDTH;
-                    pipIndex = 6-x/PIP_WIDTH;
+                    pipIndex = 6-x/PIP_WIDTH; // red home quadrant
                 }
             }
             else if (y > INNER_BOTTOM_EDGE - 5*(CHECKER_DIAMETER+3) && y < INNER_BOTTOM_EDGE) {
                 if (x > 0 && x < 6*PIP_WIDTH) {
-                    pipIndex = 13+x/PIP_WIDTH;
+                    pipIndex = 13+x/PIP_WIDTH; // white outer quadrant
                 }
                 else if (x > 6*PIP_WIDTH && x < 6*PIP_WIDTH + BAR_WIDTH) {
-                    pipIndex = 25;
+                    pipIndex = 25; // red's bar
                 }
                 else if (x > 6*PIP_WIDTH + BAR_WIDTH && x < 12*PIP_WIDTH + BAR_WIDTH) {
                     x -= 6*PIP_WIDTH + BAR_WIDTH;
-                    pipIndex = 19+x/PIP_WIDTH;
+                    pipIndex = 19+x/PIP_WIDTH; // red home quadrant
                 }
             }
             return pipIndex;
@@ -358,21 +417,24 @@ public class GUI {
         public void paintComponent(Graphics g) {
             Graphics2D g2 = (Graphics2D) g;
 
-            xp = new int[]{0, 24, 48};
-            yp = new int[]{0, 220, 0};
+            xp = new int[]{0, PIP_WIDTH/2, PIP_WIDTH};
+            yp = new int[]{0, PIP_LENGTH, 0};
+
+            int bottom = BOARD_HEIGHT-2*BEZEL;
 
             for (int i = 0; i < 24; i++) {
-                if (i==12) {
-                    xp = new int[]{0, 24, 48};
-                    yp = new int[]{560, 340, 560};
+                if (i==12) { // change to coordinates for the bottom pips
+                    xp = new int[]{0, PIP_WIDTH/2, PIP_WIDTH};
+                    yp = new int[]{bottom, bottom-PIP_LENGTH, bottom};
                 }
-                g2.setColor((i%2 == 0 && i < 12) || (i%2 == 1 && i >= 12) ? WHITE_PIP : RED_PIP);
+                g2.setColor((i%2 == 0 && i < 12) || (i%2 == 1 && i >= 12) ? WHITE_PIP : RED_PIP); // alternate between white and red
                 g2.fillPolygon(xp, yp, 3);
+                // Move to next pip
                 xp[0] += PIP_WIDTH;
                 xp[1] += PIP_WIDTH;
                 xp[2] += PIP_WIDTH;
 
-                if (i%12 == 5) {
+                if (i%12 == 5) { // jump over the bar
                     xp[0] += BAR_WIDTH;
                     xp[1] += BAR_WIDTH;
                     xp[2] += BAR_WIDTH;
@@ -381,6 +443,9 @@ public class GUI {
         }
     }
 
+    /**
+     * Paints the dice.
+     */
     public class DiceFacesP extends JPanel {
 
         ArrayList<Integer> diceValues = new ArrayList<>(Arrays.asList(1,1));
@@ -395,8 +460,13 @@ public class GUI {
             setOpaque(false);
         }
 
+        /**
+         * Paints new dice on the board.
+         *
+         * @param dv A list of new dice values
+         */
         public void updateDiceValues(ArrayList<Integer> dv) {
-            diceValues = new ArrayList<>(dv);
+            diceValues = new ArrayList<>(dv); // make sure that it is a copy and not a reference
             repaint();
         }
 
@@ -422,7 +492,7 @@ public class GUI {
             int spotTop = D1SpotTop;
             int tr = 2*spotSize;
             for (int numSpots : diceValues) {
-                switch (numSpots) {
+                switch (numSpots) { // exploit that spots on die 1 is a subset of die 3 which is a subset of die 5
                     case 5:
                         g2.fillRect(spotLeft, spotTop, spotSize, spotSize);
                         g2.fillRect(spotLeft + 2*tr, spotTop + 2*tr, spotSize, spotSize);
@@ -448,6 +518,9 @@ public class GUI {
         }
     }
 
+    /**
+     * Paints roll dice button
+     */
     public class RollDiceP extends JPanel {
         public RollDiceP() {
             setBounds( DICE_TRAY_LEFT,  DICE_TRAY_TOP + DICE_TRAY_HEIGHT + 20,  DICE_TRAY_WIDTH,  DICE_TRAY_WIDTH );
@@ -469,14 +542,25 @@ public class GUI {
             this.add(textDice, c);
         }
 
+        /**
+         * Checks if the button is clicked or not.
+         *
+         * @param xp The x coordinate
+         * @param yp The y coordinate
+         * @return True if button is clicked : False otherwise
+         */
         public boolean isMouseOn(int xp, int yp) {
-            if (xp > DICE_TRAY_LEFT && xp < DICE_TRAY_RIGHT && yp > DICE_TRAY_TOP + DICE_TRAY_HEIGHT + 20 && yp < DICE_TRAY_TOP + DICE_TRAY_HEIGHT + DICE_TRAY_WIDTH) {
+            if (xp > DICE_TRAY_LEFT && xp < DICE_TRAY_RIGHT && yp > DICE_TRAY_TOP + DICE_TRAY_HEIGHT + 20 &&
+                    yp < DICE_TRAY_TOP + DICE_TRAY_HEIGHT + DICE_TRAY_WIDTH) {
                 return true;
             }
             return false;
         }
     }
 
+    /**
+     * Displays the current player.
+     */
     public class ActivePlayerP extends JPanel {
 
         JLabel currentPlayer;
